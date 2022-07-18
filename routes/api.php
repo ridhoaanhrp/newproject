bc40ec0a38eeb6d73f1f7bdddd3a08349afba319
rifqrmdhn <103649667+rifqrmdhn@users.noreply.github.com>
2022-07-18 12:25:27 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\DosenController;
+use App\Http\Controllers\ProgressController;
 
 /*
 |--------------------------------------------------------------------------
